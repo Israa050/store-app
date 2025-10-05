6ec098a858e28a107c5de9c9edaa83271a199118
Israa Essa <esraelshbly@gmail.com>
2025-10-05 13:22:15 +0200

    added initial project setup added required packages added intial project folders

---
+++ lib/core/helpers/spacing.dart
@@ -0,0 +1,17 @@
+
+
+import 'package:flutter/material.dart';
+
+Widget horizontalPadding(double width){
+  return SizedBox(
+    height: width,
+  );
+}
+
+
+
+Widget verticalPadding(double height){
+  return SizedBox(
+    height: height,
+  );
+}
---
+++ lib/core/themes/styles/styles.dart
@@ -0,0 +1,63 @@
+
+
+// import 'package:flutter/material.dart';
+// import 'package:google_fonts/google_fonts.dart';
+// import 'font_weight_helper.dart';
+
+// class TextStyles {
+
+//    static TextStyle base({
+//     required double size,
+//     required FontWeight fontWeight,
+//     required Color color,
+//   }) {
+//     return GoogleFonts.raleway(
+//       fontSize: size,
+//       fontWeight: fontWeight,
+//       color: color,
+//     );
+//   }
+
+//   static TextStyle font24DarkPurpuleBold =
+//       base(size: 24, fontWeight: FontWeightHelper.bold, color: AppColors.darkPurple);
+
+//   static TextStyle font14WhiteBold =
+//       base(size: 14, fontWeight: FontWeightHelper.bold, color: Colors.white); 
+
+//   static TextStyle font12BlackSemiBold =
+//       base(size: 12, fontWeight: FontWeightHelper.semiBold, color: Colors.black); 
+
+//   static TextStyle font14DarkPurpleBold =
+//       base(size: 14, fontWeight: FontWeightHelper.bold, color: AppColors.darkPurple); 
+
+//   static TextStyle font12GreyMedium =
+//       base(size: 12, fontWeight: FontWeightHelper.medium, color: AppColors.grey); 
+      
+//   static TextStyle font16DarkPurpleSemiBold =
+//       base(size: 16, fontWeight: FontWeightHelper.semiBold, color: AppColors.darkPurple);          
+
+//   static TextStyle font14GreySemiBold =
+//       base(size: 14, fontWeight: FontWeightHelper.semiBold, color: AppColors.grey);      
+
+//   static TextStyle font14WhiteSemiBold =
+//       base(size: 14, fontWeight: FontWeightHelper.semiBold, color: Colors.white);
+
+//   static TextStyle font13WhiteMedium =
+//       base(size: 13, fontWeight: FontWeightHelper.medium, color: Colors.white); 
+
+//   static TextStyle font14WhiteRegular =
+//       base(size: 14, fontWeight: FontWeightHelper.regular, color: Colors.white);          
+
+//   static TextStyle font14WhiteMedium =
+//       base(size: 14, fontWeight: FontWeightHelper.medium, color: Colors.white);    
+
+//    static TextStyle font22DarkPurpleBold =
+//       base(size: 22, fontWeight: FontWeightHelper.bold, color: AppColors.darkPurple); 
+
+//    static TextStyle font16WhiteBold =
+//       base(size: 16, fontWeight: FontWeightHelper.bold, color: Colors.white);
+
+//    static TextStyle font12LightPurpleBold =
+//       base(size: 12, fontWeight: FontWeightHelper.bold, color: AppColors.light);                                       
+
+// }
